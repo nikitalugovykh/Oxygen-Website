cf1cf430cc679731a6686a42dcbc42b579e5ca7f
Nikita Lugovykh <nikita_lugovykh@mail.ru>
2021-08-10 12:36:53 +0200

    ready player with base configuration

--- components/video/video.js
+++ components/video/video.js
@@ -1,24 +1,62 @@
 const video = document.querySelector('.video');
+const video__btn = document.querySelector('.video__btn');
 const video__container = document.querySelector('.video__container');
+const video__content = document.querySelector('.video__content');
 const video__popup = document.querySelector('.video-popup');
 const video__popup__content = document.querySelector('.video-popup__content');
+const video_popup__close = document.querySelector('.video-popup__close');
 
 
-video__container.addEventListener('click', () => {
-  video__popup.classList.toggle('visible');
+
+window.addEventListener('resize', () => {
+  video_popup__close.style.opacity = 0;
+  const scrollPositionY = pageYOffset;
+  const containerOffsetTop = video__container.offsetTop;
+  const windowHeight = document.documentElement.clientHeight;
+  if (video__popup.classList.contains('visible')) {
+
+    video__popup.style.top = - (containerOffsetTop - scrollPositionY) + windowHeight/2 + 'px'; 
+
+  }
+  setTimeout(()=> {
+    const video__popupHeight = video__popup__content.getBoundingClientRect().height;
+    video_popup__close.style.top =  windowHeight/2 - video__popupHeight/2 + 'px';
+    video_popup__close.style.right = 3 + '%';
+    video_popup__close.style.opacity = 1;
+  },1000);
+})
+
+window.addEventListener('click', (event) => {
+  if (event.target === video__content) {
+    video__popup.classList.toggle('visible');
+
+    appearanceVideoPlayerOnFullscreen()
+    settingVideoPlayer(video__content, video__popup__content)
+  } else {
+    video__popup.classList.toggle('visible');
+    appearanceVideoPlayerOnFullscreen();
+    settingVideoPlayer(video__popup__content, video__content)
+  }
+
+})
+
+
+function appearanceVideoPlayerOnFullscreen () {
   const scrollPositionY = pageYOffset;
   const scrollPositionX = pageXOffset;
   const containerOffsetTop = video__container.offsetTop;
   const containerOffsetLeft = video.offsetLeft;
   const windowWidth = document.documentElement.clientWidth;
   const windowHeight = document.documentElement.clientHeight;
+  video_popup__close.style.opacity = 0;
 
   if (video__popup.classList.contains('visible')) {
 
+    video__content.style.opacity = 0.3;
+    video__btn.style.display = 'none';
     video__popup.style.top = - (containerOffsetTop - scrollPositionY) + windowHeight/2 + 'px';
-
     video__popup.style.left = 50 + '%';
-    video__popup.style.width = 95 + 'vw';
+    video__popup.style.width = 83 + 'vw';
     video__popup.style.height = 95 + 'vh';
     video__popup.style.transform = 'translate3d(-50%, -50%, 0)';
     document.documentElement.style.backgroundColor = 'rgba(31, 31, 31, 0.9)';
@@ -27,13 +65,19 @@ video__container.addEventListener('click', () => {
     video__popup__content.style.top = 50 + '%';
     video__popup__content.style.left = 50 + '%';
     video__popup__content.style.transform = 'translate3d(-50%, -50%, 0)';
-s
     document.documentElement.style.overflowX = 'hidden';
     document.documentElement.style.overflowY = 'hidden';
 
+    setTimeout(()=> {
+      const video__popupHeight = video__popup__content.getBoundingClientRect().height;
+      video_popup__close.style.top =  windowHeight/2 - video__popupHeight/2 + 'px';
+      video_popup__close.style.right = 3 + '%';
+      video_popup__close.style.opacity = 1;
+    },1000);
 
   } else {
-
+    video__content.style.opacity = 1;
+    video__btn.style.display = 'block';
     video__popup.style.top = '50%';
     video__popup.style.left = '50%';
     video__popup.style.width = 100 + '%';
@@ -43,16 +87,14 @@ s
 
     document.documentElement.style.overflowX = 'visible';
     document.documentElement.style.overflowY = 'visible';
+    video_popup__close.style.opacity = 0;
   }
-})
+}
 
-window.addEventListener('resize', () => {
-  const scrollPositionY = pageYOffset;
-  const containerOffsetTop = video__container.offsetTop;
-  const windowHeight = document.documentElement.clientHeight;
-  console.log('lol');
-  if (video__popup.classList.contains('visible')) {
-
-    video__popup.style.top = - (containerOffsetTop - scrollPositionY) + windowHeight/2 + 'px'; 
-  }
-})
+function settingVideoPlayer(videobase, popup) {
+  let timeShownVideo = videobase.currentTime;
+  videobase.pause()
+  
+  popup.currentTime = timeShownVideo;
+  popup.play()
+}
