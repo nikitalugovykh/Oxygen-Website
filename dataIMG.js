b2da859ee70584e2022d4cb6fc9c16c5dc259e6a
Nikita Lugovykh <nikita_lugovykh@mail.ru>
2021-08-17 08:17:11 +0200

    optimized pictures and videos

--- dataIMG.js
+++ dataIMG.js
@@ -1,158 +1,158 @@
 
 dataImg  = [
     {
-        name: '1.jpg',
-        srcHighQuality : 'high-quality/1.jpg',
+        name: '1.webp',
+        srcHighQuality : 'high-quality/1.webp',
         description:  'Man is doing acrobatic element',
         tags : ['human']
     },
     {
-        name: '2.jpg',
-        srcHighQuality : 'high-quality/2.jpg',
+        name: '2.webp',
+        srcHighQuality : 'high-quality/2.webp',
         description: 'Futuristic statue outside Dongdamum Design Plaza',
         tags : ['art']
     },
     {
-        name: '3.jpg',
-        srcHighQuality : 'high-quality/3.jpg',
+        name: '3.webp',
+        srcHighQuality : 'high-quality/3.webp',
         description: 'Editorial, Business & Work',
         tags : ['art', 'human']
     },
     {
-        name: '4.jpg',
-        srcHighQuality : 'high-quality/4.jpg',
+        name: '4.webp',
+        srcHighQuality : 'high-quality/4.webp',
         description: 'On top of the mountain',
         tags : ['nature', 'human']
     },
     {
-        name: '5.jpg',
-        srcHighQuality : 'high-quality/5.jpg',
+        name: '5.webp',
+        srcHighQuality : 'high-quality/5.webp',
         description: 'walkin in bandung sidewalk',
         tags : ['city', 'human']
     },
     {
-        name: '6.jpg',
-        srcHighQuality : 'high-quality/6.jpg',
+        name: '6.webp',
+        srcHighQuality : 'high-quality/6.webp',
         description: 'Arts & Culture',
         tags : ['art', 'city']
     },
     {
-        name: '7.jpg',
-        srcHighQuality : 'high-quality/7.jpg',
+        name: '7.webp',
+        srcHighQuality : 'high-quality/7.webp',
         description: 'Blue Parrot',
         tags : ['animal', 'nature']
     },
     {
-        name: '8.jpg',
-        srcHighQuality : 'high-quality/8.jpg',
+        name: '8.webp',
+        srcHighQuality : 'high-quality/8.webp',
         description: 'Cafe interior',
         tags : ['art', 'city']
     },
     {
-        name: '9.jpg',
-        srcHighQuality : 'high-quality/9.jpg',
+        name: '9.webp',
+        srcHighQuality : 'high-quality/9.webp',
         description: 'Catrina´s parade in México City',
         tags : ['art', 'human']
     },
     {
-        name: '10.jpg',
-        srcHighQuality : 'high-quality/10.jpg',
+        name: '10.webp',
+        srcHighQuality : 'high-quality/10.webp',
         description: 'Fade',
         tags : ['nature']
     },
     {
-        name: '11.jpg',
-        srcHighQuality : 'high-quality/11.jpg',
+        name: '11.webp',
+        srcHighQuality : 'high-quality/11.webp',
         description: 'Morning at the Starfield Library, COEX Korea',
         tags : ['city']
     },
     {
-        name: '12.jpg',
-        srcHighQuality : 'high-quality/12.jpg',
+        name: '12.webp',
+        srcHighQuality : 'high-quality/12.webp',
         description: 'Graffiti',
         tags : ['art', 'city']
     },
     {
-        name: '13.jpg',
-        srcHighQuality : 'high-quality/13.jpg',
+        name: '13.webp',
+        srcHighQuality : 'high-quality/13.webp',
         description: 'Walking in the town',
         tags : ['human', 'city']
     },
     {
-        name: '14.jpg',
-        srcHighQuality : 'high-quality/14.jpg',
+        name: '14.webp',
+        srcHighQuality : 'high-quality/14.webp',
         description: 'Editorial, Arts & Culture',
         tags : ['art', 'human']
     },
     {
-        name: '15.jpg',
-        srcHighQuality : 'high-quality/15.jpg',
+        name: '15.webp',
+        srcHighQuality : 'high-quality/15.webp',
         description: 'Interiors',
         tags : ['city', 'art']
     },
     {
-        name: '16.jpg',
-        srcHighQuality : 'high-quality/16.jpg',
+        name: '16.webp',
+        srcHighQuality : 'high-quality/16.webp',
         description: 'Majestic pose of Royal Bengal Tiger taken at Banerghatta National Park.',
         tags : ['nature', 'animal']
     },
     {
-        name: '17.jpg',
-        srcHighQuality : 'high-quality/17.jpg',
+        name: '17.webp',
+        srcHighQuality : 'high-quality/17.webp',
         description: 'stag in the highlands',
         tags : ['nature', 'animal']
     },
     {
-        name: '18.jpg',
-        srcHighQuality : 'high-quality/18.jpg',
+        name: '18.webp',
+        srcHighQuality : 'high-quality/18.webp',
         description: 'forrest in sweden green wood bog',
         tags : ['nature']
     },
     {
-        name: '19.jpg',
-        srcHighQuality : 'high-quality/19.jpg',
+        name: '19.webp',
+        srcHighQuality : 'high-quality/19.webp',
         description: 'Startled blue-eyed cat',
         tags : ['nature', 'animal']
     },
     {
-        name: '20.jpg',
-        srcHighQuality : 'high-quality/20.jpg',
+        name: '20.webp',
+        srcHighQuality : 'high-quality/20.webp',
         description: 'Roses Tattoo',
         tags : ['art', 'human']
     },
     {
-        name: '21.jpg',
-        srcHighQuality : 'high-quality/21.jpg',
+        name: '21.webp',
+        srcHighQuality : 'high-quality/21.webp',
         description: 'Joker',
         tags : ['art', 'city']
     },
     {
-        name: '22.jpg',
-        srcHighQuality : 'high-quality/22.jpg',
+        name: '22.webp',
+        srcHighQuality : 'high-quality/22.webp',
         description: 'Statue in gardens at Versailles.',
         tags : ['art', 'city']
     },
     {
-        name: '23.jpg',
-        srcHighQuality : 'high-quality/23.jpg',
+        name: '23.webp',
+        srcHighQuality : 'high-quality/23.webp',
         description: 'Rainy hike near Eklutna Lake, AK.',
         tags : ['nature']
     },
     {
-        name: '24.jpg',
-        srcHighQuality : 'high-quality/24.jpg',
+        name: '24.webp',
+        srcHighQuality : 'high-quality/24.webp',
         description: 'Taking The Scenic Route',
         tags : ['nature']
     },
     {
-        name: '25.jpg',
-        srcHighQuality : 'high-quality/25.jpg',
+        name: '25.webp',
+        srcHighQuality : 'high-quality/25.webp',
         description: 'La Naissance des Beaux - Arts',
         tags : ['art', 'city']
     },
     {
-        name: '26.jpg',
-        srcHighQuality : 'high-quality/26.jpg',
+        name: '26.webp',
+        srcHighQuality : 'high-quality/26.webp',
         description: "Campo de' Fiori, Rome, Italy in the rain during lockdown",
         tags : ['city']
     }
